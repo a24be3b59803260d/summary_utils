ab5d6655ab0bb4f643da872e0380d2b189d601fd
a24be3b5 9803260d <a24be3b5.9803260d@gmail.com>
2019-04-11 16:53:06 -0400

    added additional node and edge types and new "colorful" view for displaying them

--- tweet_archives_to_graphml.py
+++ tweet_archives_to_graphml.py
@@ -5,13 +5,27 @@ import xml.etree.cElementTree as et
 
 class TwitterArchiveGrapher:
 
-    def __init__(self, name):
-        self.graph_name = name
+    def __init__(self, args):
+        self.graph_name = args.graph_name
+        self.mentions = args.mentions
+        self.urls = args.urls
+        self.tlds = args.tlds
+        self.verbose = args.verbose
+        if args.all:
+            self.mentions = True
+            self.urls = True
+            self.tlds = True
+
+        if self.verbose:
+            print("Args: %s" % args)
+
         self.users = dict()
         self.tweets = dict()
         self.entities = dict()
         self.entities['hashtags'] = set()
         self.entities['symbols'] = set()
+        self.entities['urls'] = dict()
+        self.entities['tlds'] = set()
         self.edge_attr_keys = dict()
         self.node_attr_keys = dict()
 
@@ -82,6 +96,7 @@ class TwitterArchiveGrapher:
             id="tweet_id:%s" % tweet["id_str"])
 
         et.SubElement(se, 'data', key='entity_type').text = "tweet"
+        et.SubElement(se, 'data', key='name').text = tweet['nice_label']
 
         for k in tweet:
             et.SubElement(
@@ -147,11 +162,59 @@ class TwitterArchiveGrapher:
                 ese, 'data',
                 key='interaction').text = "used_symbol"
 
-        # TODO handle user mentions
-        # user_mentions
+        # handle user mentions
+        if self.mentions:
+            for e in tweet['entities']['user_mentions']:
 
-        # TODO hand URL nodes and edges
-        # urls
+                ese = et.SubElement(
+                    graph, "edge",
+                    source="tweet_id:%s" % tweet['id_str'],
+                    target="user_id:%s" % e['id_str'])
+
+                et.SubElement(
+                    ese, 'data',
+                    key='entity_type').text = "user_mention"
+
+                et.SubElement(
+                    ese, 'data',
+                    key='interaction').text = "user_mention"
+
+        # handle URLs (TLDs below) and edges
+        if self.urls:
+            for e in self.entities['urls']:
+
+                ese = et.SubElement(
+                    graph, "edge",
+                    source="tweet_id:%s" % tweet['id_str'],
+                    target="url:%s" % e)
+
+                et.SubElement(
+                    ese, 'data',
+                    key='entity_type').text = "linked_url"
+
+                et.SubElement(
+                    ese, 'data',
+                    key='interaction').text = "linked_url"
+
+        # handle TLDs edges
+        if self.tlds:
+            for tld in self.entities['tlds']:
+
+                # ignore twitter.com for sanity
+                if "twitter.com" != tld:
+
+                    ese = et.SubElement(
+                        graph, "edge",
+                        source="tweet_id:%s" % tweet['id_str'],
+                        target="tld:%s" % tld)
+
+                    et.SubElement(
+                        ese, 'data',
+                        key='entity_type').text = "linked_tld"
+
+                    et.SubElement(
+                        ese, 'data',
+                        key='interaction').text = "linked_tld"
 
     # extract user(s) from tweet and add them to user dict
     def add_tweets(self, tweets):
@@ -185,9 +248,11 @@ class TwitterArchiveGrapher:
 
     # convert stored tweets to graphml format
     def to_graphml(self):
-        graphml = et.Element('graphml', xmlns="http://graphml.graphdrawing.org/xmlns")
+        graphml = et.Element(
+            'graphml', xmlns="http://graphml.graphdrawing.org/xmlns")
         graphml.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        graphml.set("xsi:schemaLocation", "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
+        graphml.set("xsi:schemaLocation", "http://graphml.graphdrawing.org/xmlns \
+            http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
 
         # generate node attributes
         for k in self.edge_attr_keys:
@@ -212,7 +277,34 @@ class TwitterArchiveGrapher:
             graphml, 'graph', id=self.graph_name, edgedefault="directed")
 
         ###################
-        # create nodes and edges
+        # create nodes BEFORE edges
+
+        # hashtag, symbol, user_mention, url, tld nodes
+        for tweet_id in self.tweets:
+            # self.entities['hashtags'].add(e['text'])
+            tweet = self.tweets[tweet_id]
+            hashtags = [x['text'] for x in tweet['entities']['hashtags']]
+            self.entities['hashtags'].update(hashtags)
+            symbols = [x['text'] for x in tweet['entities']['symbols']]
+            self.entities['symbols'].update(symbols)
+
+            if self.mentions:
+                # add any missing users from mentions by tweet
+                # since we have already extracted all users we only need
+                # add mention (partial user info) for those missing keys
+                for um in tweet['entities']['user_mentions']:
+                    if um['id_str'] not in self.users:
+                        self.upsert_user(um)
+
+            # extract urls and TLDs
+            for url in tweet['entities']['urls']:
+                # using expanded url as key (this might be dangerous)
+                if self.urls:
+                    self.entities['urls'][url['expanded_url']] = url
+
+                if self.tlds:
+                    tld = url['display_url'].split('/')[0]
+                    self.entities['tlds'].add(tld)
 
         # user nodes
         for user_id in self.users:
@@ -229,15 +321,6 @@ class TwitterArchiveGrapher:
                     key=k
                 ).text = str(user[k])
 
-        # hashtag and symbol nodes
-        for tweet_id in self.tweets:
-            # self.entities['hashtags'].add(e['text'])
-            tweet = self.tweets[tweet_id]
-            hashtags = [x['text'] for x in tweet['entities']['hashtags']]
-            symbols = [x['text'] for x in tweet['entities']['symbols']]
-            self.entities['hashtags'].update(hashtags)
-            self.entities['symbols'].update(symbols)
-
         # hashtags
         for ht in self.entities['hashtags']:
             se = et.SubElement(
@@ -252,9 +335,27 @@ class TwitterArchiveGrapher:
                 id="symbol:%s" % s)
             et.SubElement(se, 'data', key='entity_type').text = "symbol"
 
-        # TODO urls
+        # urls
+        if self.urls:
+            for u in self.entities['urls']:
+                se = et.SubElement(
+                    graph, "node",
+                    id="url:%s" % u)
+                et.SubElement(se, 'data', key='entity_type').text = "url"
+
+        # TLDs
+        if self.tlds:
+            for tld in self.entities['tlds']:
+
+                # ignore twitter.com for sanity
+                if "twitter.com" != tld:
+                    se = et.SubElement(
+                        graph, "node",
+                        id="tld:%s" % tld)
+                    et.SubElement(se, 'data', key='entity_type').text = "tld"
 
         # TODO mentions
+        # this might be handled by updated user code
 
         # tweet nodes and edges
         for tweet_id in self.tweets:
@@ -270,6 +371,15 @@ class TwitterArchiveGrapher:
     def get_num_users(self):
         return len(self.users.keys())
 
+    def get_num_hashtags(self):
+        return len(self.entities['hashtags'])
+
+    def get_num_urls(self):
+        return len(self.entities['urls'])
+
+    def get_num_tlds(self):
+        return len(self.entities['tlds'])
+
 
 def main():
     parser = ArgumentParser()
@@ -279,12 +389,21 @@ def main():
     parser.add_argument("-g", "--graph-name", dest='graph_name',
                         default="Default Graph Name",
                         help="GraphML network name.")
+    parser.add_argument("-m", "--mentions", action="store_true",
+                        help="Add nodes and edges for mentions")
+    parser.add_argument("-u", "--urls", action="store_true",
+                        help="Add nodes and edges for full urls")
+    parser.add_argument("-t", "--tlds", action="store_true",
+                        help="Add nodes and edges for TLDs")
+    parser.add_argument("-a", "--all", action="store_true",
+                        help="Node and edges for everything. \
+                             WARNING! This creates very large graph files")
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument('infile_paths', nargs='+', metavar='INFILE')
 
     args = parser.parse_args()
 
-    tager = TwitterArchiveGrapher(args.graph_name)
+    tager = TwitterArchiveGrapher(args)
 
     for infile in args.infile_paths:
         tweets = [json.loads(t) for t in open(
@@ -302,7 +421,9 @@ def main():
 
     print("Tweets Processed: %d" % tager.get_num_processed_tweets())
     print("Users extracted: %d" % tager.get_num_users())
-
+    print("Hashtags extracted: %d" % tager.get_num_hashtags())
+    print("TLDs extracted: %d" % tager.get_num_tlds())
+    print("Urls extracted: %d" % tager.get_num_urls())
 
 if __name__ == "__main__":
     main()
